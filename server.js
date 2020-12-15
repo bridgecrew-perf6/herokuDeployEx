d665fd67d4d9f3382cf5a700b7a21d528df55c77
Patty C <patrickcorcoran10@gmail.com>
2020-12-15 17:17:56 -0600

    commit with updated server

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+  });
+
 app.listen(PORT, function() {
     console.log("We are listening on PORT: ", PORT)
 })
