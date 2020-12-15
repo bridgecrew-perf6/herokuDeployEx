9b26a718cbe485c40429c5ef3da8c970a4ac0373
Patty C <patrickcorcoran10@gmail.com>
2020-12-15 17:23:20 -0600

    commit time

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = 3000 || process.env.PORT;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
 app.get("/", function(req, res) {
     res.json(path.join(__dirname, "public/index.html"));
